92e5f7da764a2d2a6b305e4493e5994b7da016a8
V_SNIPE <94768886+VINNOVER@users.noreply.github.com>
2025-04-11 13:24:45 +0100

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+F  * Name: Vianney Goetrand KEDJITAR
+  * Graphic Designer & Web Developer
+  * bestinnover01@gmail.com
+  * https://www.linkedin.com/in/vianney-goetrand-kedjitar-0b1a4b1a6/
+  * https://www.behance.net/vinnover
---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+  /**
+  * Name: Vianney Goetrand KEDJITAR
+  * Graphic Designer & Web Developer
+  * bestinnover01@gmail.com
+  * https://www.linkedin.com/in/vianney-goetrand-kedjitar-0b1a4b1a6/
+  * https://www.behance.net/vinnover
+  */
+
+  // real receiving email address
+  $receiving_email_address = 'bestinnover01@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
